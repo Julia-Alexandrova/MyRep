cbebd5825a9a979c400b44c605495e5daa369e52
Julia-Alexandrova <julalexandrova@gmail.com>
2013-07-01 04:36:21 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
    	 System.out.println("Hello, World");
 //	 System.out.println(args[0]);
-	System.out.println("from Julia")
+	System.out.println("hello again")
 	 
 }
 }
